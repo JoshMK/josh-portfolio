b6f15a3daacd1a08f6dd1a05e7807446a1100f73
Joshua <joshuakirwin@gmail.com>
2019-04-10 22:58:32 -0500

    more prototyping.

--- src/components/projectSummary/ProjectSummary.js
+++ src/components/projectSummary/ProjectSummary.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 import './ProjectSummary.scss';
 
 class ProjectSummary extends Component {
     render() {
         return (
-            <div>
-                <h2>Jesus Always Existed</h2>
-                <img className='app__project-image' src={require(`../../images/${this.props.projectImage}.png`)} alt=''/>
-            </div>
+            <section className='app__project-summary'>
+                <div className='app__project-image-container'>
+                    <h2 className='app__project-title'>
+                        <Link className='app__project-nav' to='../projects'>Projects</Link> / Jesus Always Existed</h2>
+                    <img
+                        className='app__project-image'
+                        src={require(`../../images/${this.props.projectImage}.png`)}
+                        alt=''/>
+                </div>
+                <div className='app__project-text-container'>
+                <a className='app__project-web-link' href='https://jesusalwaysexisted.com/' target='_blank'>Visit Website</a>
+                <p>Tech used:</p>
+                </div>
+            </section>
         );
     }
 }
